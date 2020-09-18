c9cc7664dbf781267a48168ec97deee4bb1f0f3d
QDG <nasha@QDGs-MacBook-Air.local>
2020-09-18 11:08:16 -0700

    added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+
 import React from "react";
+import { Route, Switch } from 'react-router-dom';
+
 
 const App = () => {
+
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      
+     <Switch>
+
+     <Route path="/pizza/">
+        <PizzaForm />
+      </Route>
+
+      <Route path="/">
+        <PizzaHome />
+      </Route>
+
+     
+      </Switch>
+      
     </>
   );
 };
