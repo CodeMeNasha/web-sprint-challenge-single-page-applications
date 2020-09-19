77d70984656a50c34adbbd4470ea21fa80cd6ee7
QDG <nasha@QDGs-MacBook-Air.local>
2020-09-19 10:51:03 -0700

    added validations and set up form

--- src/Components/PizzaHome.js
+++ src/Components/PizzaHome.js
@@ -1,13 +1,39 @@
-import React from "react";
-import Pizza from "../images/Pizza.jpg"
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Pizza from "../images/Pizza.jpg";
+import axios from "axios";
+import * as yup from "yup";
+
+const formSchema = yup.object().shape({
+
+    name: yup
+    .string()
+    .required("Please provide your full name"),
+
+    email: yup
+    .string()
+    .email("Please provide a valid email address")
+    .required("Must include a valid email address")
+
+})
 
 const PizzaHome = () => {
+  return (
+    <>
+      <img src={Pizza} />
 
-    return (
-      <>
-       <img src={Pizza}/>
-        
-      </>
-    );
-  };
-  export default PizzaHome;
+      <form>
+        <label>
+          <input id="name" type="text" name="name"></input>
+        </label>
+
+        <label>
+          <input id="email" type="text" name="email"></input>
+        </label>
+
+        <button>Order Now</button>
+      </form>
+    </>
+  );
+};
+export default PizzaHome;
